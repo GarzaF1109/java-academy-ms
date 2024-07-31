46818f84f59623c293287f4df52879872515b1ad
GarzaF1109 <fernandogarzamedina3@gmail.com>
2024-07-31 13:17:23 -0600

    Hice un Micro Servicio con conexión a config server

---
+++ Java-Academy/src/test/java/com/FGM/Java/Academy/JavaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FGM.Java.Academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
